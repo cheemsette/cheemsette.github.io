9a3a26138d50c51009b7056de0df160183661643
cheemsette <90909808+cheemsette@users.noreply.github.com>
2021-11-04 19:02:55 +0100

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,14 @@
-// Made by Lummit - https://obnoxious.club/ | https://github.com/Lumm1t/ | Discord: Lummit#0201
-// Credits to expl0it, shellcode.team
-// GitHub: https://github.com/Lumm1t/obnoxious.club
-
 class _app {
   id = 0;
   videoElement = null;
   audioElement = null;
-  musicVolume = 0.12;
+  musicVolume = 0.00;
   musicFadeIn = 4000;
   skippedIntro = false;
   backgroundToggler = false;
   shouldIgnoreVideo = false;
   effects = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing', 'tada', 'wobble', 'jello'];
-  brandDescription = ['11 years old kids team', 'pro roblox players', 'xane ddosers', 'trashtalkers', 'esoterik fanboys', 'hack vs hack team'];
+  brandDescription = ['toxic', 'shdwrv', 'regular in ur moms bed', 'pro hacker 1337', 'femboy'];
 
   titleChanger = (text, delay) => {
     if (!text) return;
